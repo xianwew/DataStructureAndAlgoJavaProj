8a72374ede1b66f15c08422c5b23202a178d728b
Jiren1247 <jiren@vt.edu>
2023-09-09 11:31:46 +0800

    parsertest

--- HW1/F23P1starter/src/ParserTest.java
+++ HW1/F23P1starter/src/ParserTest.java
@@ -9,7 +9,13 @@ public class ParserTest {
 	
 		Parser test = new Parser();
 		String[] args = {"Java", "SemManager", "-1", "0", "0"};
-		assertEquals(null, test.initializeComponents(args));
+//		assertEquals(null, test.initializeComponents(args));
+		
+		assertEquals(1,test.getInstruction("insert 10000"));
+		assertEquals(2,test.getInstruction("search 10000"));
+		assertEquals(3,test.getInstruction("print  hashtable"));
+		assertEquals(4,test.getInstruction("print  blocks"));
+		assertEquals(5,test.getInstruction("delete  10000"));
 	}
 
 }
