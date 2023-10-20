6cf2519fea9a439d1c6701225c83e256ab30375e
xianwew <50687225+xianwew@users.noreply.github.com>
2023-10-20 19:22:38 -0400

    123

--- HW3/F23P3starter/src/BufferPool.java
+++ HW3/F23P3starter/src/BufferPool.java
@@ -4,17 +4,12 @@ import java.io.IOException;
 public class BufferPool implements BufferPoolADT {
     public class BufferList {
         private BufferList next;
-        private BufferList previous;
         private Buffer buffer;
 
         public BufferList(boolean createBuffer) {
             if(createBuffer) {
                 this.buffer = new Buffer();
             }
-        }
-
-        public BufferList(Buffer bf) {
-            this.buffer = bf;
         }
  
         public BufferList getNext() {
@@ -56,16 +51,48 @@ public class BufferPool implements BufferPoolADT {
         }
         
         for(int i = 0; i < 4; i++) {
-            insertBlock.getBuffer().getData()[i + pos] = space[i];
+            insertBlock.getBuffer().getData()[i + pos - blockID * 4096] = space[i];
         }
     }
 
     public void getbytes(byte[] space, int sz, int pos) {
-
+        int blockID = pos / 4096;
+        BufferList searchBlock = dummy.getNext();
+        while(searchBlock != null && searchBlock.getBuffer().getID() != blockID) {    
+            searchBlock = searchBlock.getNext();
+        }
+        
+        if(searchBlock != null) {
+            for(int i = 0; i < 4; i++) {
+                space[i] = searchBlock.getBuffer().getData()[i + pos - blockID * 4096];
+            }
+        }   
+        else {
+            
+        }
+        
+    }
+    
+    private void setDirtyBit(int pos) {
+        int blockID = pos / 4096;
+        BufferList searchBlock = dummy.getNext();
+        while(searchBlock != null && searchBlock.getBuffer().getID() != blockID) {    
+            searchBlock = searchBlock.getNext();
+        }
+        if(searchBlock != null){
+            searchBlock.getBuffer().setDirty(true);
+        }
     }
 
     public void swap(int i, int j) {
-
+        byte[] tmpJ = new byte[4];
+        byte[] tmpI = new byte[4];
+        getbytes(tmpJ, 4, j);
+        getbytes(tmpI, 4, i);
+        insert(tmpJ, 4, i);
+        insert(tmpI, 4, j);
+        setDirtyBit(i);
+        setDirtyBit(j);
     }
 
     public byte[] readFromDisk(int index, String fileName) {
